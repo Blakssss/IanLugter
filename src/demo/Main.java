e945bb5f8c32d3bb6e2d264283bcb4a8745b8e9f
DatJustino <100353161+DatJustino@users.noreply.github.com>
2022-03-11 10:11:55 +0100

    hej

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,9 +1,14 @@
 package demo;
 
-public class Main {
+import java.awt.*;
 
+public class Main {
+    Color red = new Color(255, 0,0);
     public static void main(String[] args) {
         System.out.println("Jessi er en skildpadde");
         System.out.println();
+        System.out.println( "men skidepaden er hurtigere end haren i det lange løb!");
+
+
     }
 }
